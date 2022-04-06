99a87db405caff36e7eadec09979c5deb53ccb7b
joaquin <joaquin@J>
2022-04-06 19:48:39 -0300

    primer commit

---
+++ ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/ejemplos/ProyectDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.ejemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectDemoApplication.class, args);
+	}
+
+}
